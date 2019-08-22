6c2cc71a0ec4d2abc92020665595d6bf233b1dad
danil__pdr <danil.korotaev.71rus@gmail.com>
2019-08-22 14:15:17 +0300

    delete comments

--- ParserMODBUS/Program.cs
+++ ParserMODBUS/Program.cs
@@ -14,20 +14,6 @@ namespace ParserMODBUS
             string raw_log = args[0];
             string parsed_log = args[1];
 
-            raw_log = "raw_log_example2.log";
-            parsed_log = "test.xml";
-
-            //string str = "68 46 01 6A 02";
-            //var spltStr = str.Split(' ');
-            //byte[] bt = new byte[spltStr.Length];
-            //int count = 0;
-            //foreach (var b in spltStr)
-            //{
-            //    bt[count++] = Convert.ToByte(b, 16);
-            //}
-
-            //Console.WriteLine(Parser.CRC16(bt, bt.Length) == Convert.ToUInt32("8594", 16));
-            //Console.WriteLine(Parser.CRC16(bt, bt.Length));
             Parser parser = new Parser(raw_log);
             var extension = parsed_log.Substring(parsed_log.LastIndexOf('.') + 1);
             if (extension.Equals("xml"))
